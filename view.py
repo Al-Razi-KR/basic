49028f2e9beaa9b6f8444a25cd419bdb7e3b89bf
Al-Razi-KR <mohammedrazikr9419@gmail.com>
2021-01-10 19:35:30 +0800

    edits views.py

---
+++ view.py
@@ -0,0 +1,6 @@
+def menu():
+    basic_list = ["Add", "Substract", "Divide", "Power"]
+    medium_list = ["Root", "Square", "Sin", "Log"]
+    print("Basic")
+    for item in basic_list:
+        print()
