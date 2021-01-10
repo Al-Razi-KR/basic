215bfe365e148a378cf80ea8935d92d2d62eec80
chrisspeaks <chrisspeakssg@gmail.com>
2021-01-10 19:54:25 +0800

    changed basic.py

--- basic.py
+++ basic.py
@@ -18,18 +18,8 @@ def divide (x, y) {
     return result
 }
 
-def power (number, exp,i=1,power=1) {
-    if (exp==1){
-        return (main)
-    if (exp!=1) {
-        
-        while(i<= exp){
-            power=power*number
-            i=i+1
-         }
-        
-        }
-        return power
-    }
+def power (number, exp) {
+    result = number**exp
+    return result
 }
 
